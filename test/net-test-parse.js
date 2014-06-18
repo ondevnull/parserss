0ffa59943c3aa3d08e6aa38257cf893991dd2790
Jose Luis Rivas <me@ghostbar.co>
2014-06-18 00:41:43 -0500

    testing all (fixes #1)

--- test/net-test-parse.js
+++ test/net-test-parse.js
@@ -3,15 +3,15 @@
 'use strict';
 
 var should = require('should');
+var url = 'http://ghostbar.co/feed.xml';
 
 describe('Parsing a file (net)', function () {
   it('should parse a RSS from an URL', function (done) {
     this.timeout(40000);
 
     var parser = require('../index');
-    //var url = 'http://ghostbar.co/feed.xml';
 
-    parser('http://ghostbar.co/feed.xml', 10, function (err, response) {
+    parser(url, 10, function (err, response) {
       should.not.exist(err);
 
       should.exist(response);
@@ -20,5 +20,20 @@ describe('Parsing a file (net)', function () {
 
       done(err);
     });
+  });
+});
+
+describe('Parsing a file with getUrl (net)', function () {
+  it('should parse a RSS from an URL', function (done) {
+    var parser = require('../index').getUrl;
+
+    parser(url, 10, function (err, response) {
+      should.not.exist(err);
+      should.exist(response);
+      should.exist(response.meta);
+
+      done();
+    });
+
   });
 });
