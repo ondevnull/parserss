30967792f8a806c6acdbe1f55e13a2227c850101
Jose Luis Rivas <me@ghostbar.co>
2014-06-24 21:53:59 -0500

    Better describes (fixes #4)

--- test/test-getFile.js
+++ test/test-getFile.js
@@ -4,18 +4,18 @@
 
 var should = require('should');
 
-describe('Loading object.parse', function () {
-  var a = require('../index').parse;
+describe('Loading object.getFile', function () {
+  var a = require('../index').getFile;
 
   it('should create an exported module with type Object', function () {
     should.exist(a);
   });
 });
 
-describe('Sending a file to object.parse', function () {
+describe('Sending a file to object.getFile', function () {
   var parse = require('../index').getFile;
 
-  it('should parse an XML from the directory', function (done) {
+  it('should parse the file ./feed.xml', function (done) {
     parse(__dirname + '/feed.xml', 10, function (err, response) {
       should.not.exist(err);
       should.exist(response);
